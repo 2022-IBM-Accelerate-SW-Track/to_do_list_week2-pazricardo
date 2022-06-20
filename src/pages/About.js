090b988469a1b04e8f09518480f6833b10cdf78a
pazricar <pazricar@msu.edu>
2022-06-20 12:48:09 -0400

    Minor Changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/profile_pic_name.jpg";
 
 export default class About extends Component {
   render() {
@@ -11,19 +11,20 @@ export default class About extends Component {
           <div className="centered">
             <img
               className="profile_image"
-              src={profile_pic_name}
+              src={profile_pic}
               alt="Profile Pic"
             ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Ricardo Paz</div>
             <div className="brief_description">
-              Hey, My name is Ricardo Paz. I am a student at Michigan State University studying Computer Sciece
+              Hey, I am a student at Michigan State University studying Computer Sciece
               with a minor in Cognitive Science. I am thrilled to be able to share this experience with y'all.
-
-              Fun Fact: I belly-flopped off a waterfall.
+            </div>
+            <div className="brief_description">
+            Fun Fact: I belly-flopped off a waterfall.
             </div>
           </div>
         </div>
